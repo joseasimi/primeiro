4273b6a11f9f20f1c756edddf788b7fc856e1b53
Jose Alexandre Simi <joseasimi@gmail.com>
2020-05-04 18:30:52 -0300

    Initial commit

---
+++ R/colar.R
@@ -0,0 +1,13 @@
+#' Colar dois parametros
+#'
+#' Esta funcao recebe dois parametros e cola
+#'
+#' @param a primeiro parametro
+#' @param b segundo parametro
+#'
+#' @return descricao do resultado
+#'
+#' @export
+colar <- function(a, b) {
+  paste(a,b)
+}
